a854b47525138cc8f9a4769a5af18f5498198519
Spartak Metodiev <spartack@mail.bg>
2018-03-21 20:19:49 +0200

    show more/less char count

--- src/app/shared/components/read-more/read-more.component.ts
+++ src/app/shared/components/read-more/read-more.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit, Input } from '@angular/core';
     styleUrls: ['read-more.component.scss']
 })
 
-export class ReadMoreComponent {
+export class ReadMoreComponent implements OnInit {
+    @Input() inputData: any;
     isCollapsed: boolean = true;
+    hideShowHideButtons: boolean = false;
+    charCount = 60;
+
+    ngOnInit() {
+        if (this.inputData.toString().length <= this.charCount) {
+            this.hideShowHideButtons = true;
+            this.isCollapsed = false;
+        }
+    }
 }
